919473eb041a323ec58372f24b9d95ed4e275501
eadanfahey <eadan176@gmail.com>
2017-12-21 20:35:57 +0000

    Check that block nonce is valid in validate_block

--- src/blockchain.ml
+++ src/blockchain.ml
@@ -1,5 +1,6 @@
 open Core
 open Result
+open Bignum
 
 type t = Block.t list [@@deriving yojson]
 
@@ -9,10 +10,13 @@ let empty = []
 
 let validate_block curr_block prev_block =
   let open Block in
+  let hash_int = Bigint.Hex.of_string ("0x" ^ (hash curr_block)) in
   if curr_block.prevhash <> Some (hash prev_block) then
     Error "Invalid previous block hash"
   else if prev_block.timestamp > curr_block.timestamp then
     Error "Invalid timestamp"
+  else if Bigint.(hash_int > Constants.pow_target) then
+    Error "Invalid nonce"
   else
     Ok ()
 
